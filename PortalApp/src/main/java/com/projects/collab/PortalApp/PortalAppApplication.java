9b550ed41628775d9ccf823b3adb8a2fc2886998
Pradeep Kumar <pradeepkumar.swer@Pradeeps-MacBook-Pro.local>
2023-04-16 17:51:01 +0530

    Initial Repo Creation

---
+++ PortalApp/src/main/java/com/projects/collab/PortalApp/PortalAppApplication.java
@@ -0,0 +1,13 @@
+package com.projects.collab.PortalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalAppApplication.class, args);
+	}
+
+}
---
+++ PortalApp/src/test/java/com/projects/collab/PortalApp/PortalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.collab.PortalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
